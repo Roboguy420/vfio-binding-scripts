4763c5a806d5ca70e036883c111e136b0cd36a66
Roboguy420 <wahaller@hotmail.co.uk>
2025-10-23 22:10:41 +0100

    Ditto for reattach script

--- gpu-reattach.sh
+++ gpu-reattach.sh
@@ -7,6 +7,17 @@ DRIVERSAUDIO='snd_hda_intel'
 BUSVIDEO='0000:01:00.0'
 BUSAUDIO='0000:01:00.1'
 
+probe_and_check_ret() {
+  module=$1
+  failval=$2
+  modprobe -i $module
+  RETVAL=$?
+  if [ $RETVAL -ne 0 ]; then
+    echo "Failed to probe $module with exit code $RETVAL."
+    exit $failval
+  fi
+}
+
 # Override drivers with the dGPU ones
 echo -n "Echoing graphics card drivers into driver_override... "
 echo $DRIVERSVIDEO > "/sys/bus/pci/devices/$BUSVIDEO/driver_override"
@@ -22,6 +33,11 @@ echo Done!
 # Load dGPU drivers and bind them to card
 echo -n "Probing graphics card drivers... "
 modprobe -i $DRIVERSVIDEO $DRIVERSAUDIO
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+  echo "Failed to probe graphics card drivers with exit code $RETVAL."
+  exit 1
+fi
 echo Done!
 echo -n "Binding graphics card drivers to card... "
 echo $BUSVIDEO > "/sys/bus/pci/drivers/$DRIVERSVIDEO/bind"
@@ -31,9 +47,9 @@ echo Done!
 # Keep these lines uncommented if you are on NVIDIA
 # Comment these lines out otherwise
 echo -n "Probing additional graphics card drivers... "
-modprobe -i nvidia-uvm
-modprobe -i nvidia-modeset
-modprobe -i nvidia-drm
+probe_and_check_ret nvidia-uvm 2
+probe_and_check_ret nvidia-modeset 2
+probe_and_check_ret nvidia-drm 2
 echo Done!
 
 # End
